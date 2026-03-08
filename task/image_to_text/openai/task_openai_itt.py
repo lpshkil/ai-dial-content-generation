2af12283ec36831e3ce3fd4145788cda50c29e9b
Liubomyr Shkil <lpshkil@gmail.com>
2026-03-08 10:26:26 +0000

    Practice task

--- task/image_to_text/openai/task_openai_itt.py
+++ task/image_to_text/openai/task_openai_itt.py
@@ -4,7 +4,12 @@ from pathlib import Path
 from task._utils.constants import API_KEY, DIAL_CHAT_COMPLETIONS_ENDPOINT
 from task._utils.model_client import DialModelClient
 from task._models.role import Role
-from task.image_to_text.openai.message import ContentedMessage, TxtContent, ImgContent, ImgUrl
+from task.image_to_text.openai.message import (
+    ContentedMessage,
+    TxtContent,
+    ImgContent,
+    ImgUrl,
+)
 
 
 def start() -> None:
@@ -13,19 +18,61 @@ def start() -> None:
 
     with open(image_path, "rb") as image_file:
         image_bytes = image_file.read()
-    base64_image = base64.b64encode(image_bytes).decode('utf-8')
+    base64_image = base64.b64encode(image_bytes).decode("utf-8")
 
     # TODO:
     #  1. Create DialModelClient
+    dial_client = DialModelClient(
+        deployment_name="gpt-4o",
+        api_key=API_KEY,
+        endpoint=DIAL_CHAT_COMPLETIONS_ENDPOINT,
+    )
+    
+    #--------------------------------------------------------------------------
     #  2. Call client to analise image:
     #    - try with base64 encoded format
-    #    - try with URL: https://a-z-animals.com/media/2019/11/Elephant-male-1024x535.jpg
+    #--------------------------------------------------------------------------
+
+    messages = []
+
+    contented_message_base64 = ContentedMessage(
+        role=Role.USER,
+        content=[
+            TxtContent("What is in this image?"),
+            ImgContent(image_url=ImgUrl(f"data:image/png;base64,{base64_image}"))
+        ]
+    )
+
+    messages.append(contented_message_base64)
+
+    
+    response_message = dial_client.get_completion(messages=messages)
+    
+    #--------------------------------------------------------------------------
+    #  2. Call client to analise image:
+    # - try with URL: https://a-z-animals.com/media/2019/11/Elephant-male-1024x535.jpg
+    #--------------------------------------------------------------------------
+
+    messages = []
+
+    contented_message_url = ContentedMessage(
+        role=Role.USER,
+        content=[
+            TxtContent("What is in this image?"),
+            ImgContent(image_url=ImgUrl("https://a-z-animals.com/media/2019/11/Elephant-male-1024x535.jpg"))
+        ]
+    )
+
+    messages.append(contented_message_url)
+    response_message = dial_client.get_completion(messages=messages)
+
+    #    
     #  ----------------------------------------------------------------------------------------------------------------
     #  Note: This approach embeds the image directly in the message as base64 data URL! Here we follow the OpenAI
     #        Specification but since requests are going to the DIAL Core, we can use different models and DIAL Core
     #        will adapt them to format Gemini or Anthropic is using. In case if we go directly to
     #        the https://api.anthropic.com/v1/complete we need to follow Anthropic request Specification (the same for gemini)
-    raise NotImplementedError
+
 
 
 start()
--- task/image_to_text/task_dial_itt.py
+++ task/image_to_text/task_dial_itt.py
@@ -11,34 +11,80 @@ from task._models.role import Role
 
 
 async def _put_image() -> Attachment:
-    file_name = 'dialx-banner.png'
+    file_name = "dialx-banner.png"
     image_path = Path(__file__).parent.parent.parent / file_name
-    mime_type_png = 'image/png'
+    mime_type_png = "image/png"
     # TODO:
     #  1. Create DialBucketClient
+
+    dial_bucket_client = DialBucketClient(api_key=API_KEY, base_url=DIAL_URL)
+
     #  2. Open image file
+    with open(image_path, "rb") as f:
+        img_bytes = f.read()
+
     #  3. Use BytesIO to load bytes of image
+    img_buffer = BytesIO(img_bytes)
+
     #  4. Upload file with client
+    async with dial_bucket_client:
+        response = await dial_bucket_client.put_file(
+            name="dialx-banner", mime_type=mime_type_png, content=img_buffer
+        )
+
+        return Attachment(
+            title=file_name,
+            url=response.get("url"),
+            type=mime_type_png,
+        )
+
     #  5. Return Attachment object with title (file name), url and type (mime type)
-    raise NotImplementedError
 
 
 def start() -> None:
     # TODO:
     #  1. Create DialModelClient
+    dial_client = DialModelClient(
+        endpoint=DIAL_CHAT_COMPLETIONS_ENDPOINT,
+        api_key=API_KEY,
+        deployment_name="gpt-4o",
+    )
+
     #  2. Upload image (use `_put_image` method )
+    attachment = asyncio.run(_put_image())
+
     #  3. Print attachment to see result
+    print(attachment)
+
     #  4. Call chat completion via client with list containing one Message:
     #    - role: Role.USER
     #    - content: "What do you see on this picture?"
     #    - custom_content: CustomContent(attachments=[attachment])
+
+    prompt = Message(
+        role=Role.USER,
+        content="What do you see on this picture?",
+        custom_content=CustomContent(attachments=[attachment])
+    )
+
+    dial_client.get_completion(messages=[prompt])
+
     #  ---------------------------------------------------------------------------------------------------------------
     #  Note: This approach uploads the image to DIAL bucket and references it via attachment. The key benefit of this
     #        approach that we can use Models from different vendors (OpenAI, Google, Anthropic). The DIAL Core
     #        adapts this attachment to Message content in appropriate format for Model.
+
+
     #  TRY THIS APPROACH WITH DIFFERENT MODELS!
+    dial_client = DialModelClient(
+        endpoint=DIAL_CHAT_COMPLETIONS_ENDPOINT,
+        api_key=API_KEY,
+        deployment_name="gemini-2.0-flash-lite",
+    )
+    dial_client.get_completion(messages=[prompt])
+
+
     #  Optional: Try upload 2+ pictures for analysis
-    raise NotImplementedError
 
 
 start()
